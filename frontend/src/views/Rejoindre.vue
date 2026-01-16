aed8f3556e02f42bc64c5166226c72439a195a72
Kevin Guibert <kevin.gbt2805@gmail.com>
2026-01-16 11:08:24 +0100

    changes

--- frontend/src/views/Rejoindre.vue
+++ frontend/src/views/Rejoindre.vue
@@ -1,19 +1,24 @@
 <script setup>
-  // Ici vous pourrez mettre la logique de cette page plus tard
+// Ici vous pourrez mettre la logique de cette page plus tard
 </script>
 
 <template>
   <main>
     <section class="hero">
-        <h1>Tu souhaites rejoindre le tournoi ?</h1>
-        <h2>Alors inscris toi juste en dessous en rentrant toutes les informations nécessaires et participe au prochain tournoi !</h2>
+      <div class="hero-content">
+        <h1>Tu souhaites rejoindre <br>le tournoi ?</h1>
+        <h3>
+          Alors inscris toi juste en dessous en rentrant toutes les informations
+          nécessaires et participe au prochain tournoi !
+        </h3>
+      </div>
     </section>
     <section class="formulaire">
-        <label for="">Pseudo</label>
-        <label for="">Date de naissance</label>
-        <label for="">Mail</label>
-        <label for="">Pays</label>
-        <button class="btn btn-primary size-lg">S'inscrire</button>
+      <label for="">Pseudo</label>
+      <label for="">Date de naissance</label>
+      <label for="">Mail</label>
+      <label for="">Pays</label>
+      <button class="btn btn-primary size-lg">S'inscrire</button>
     </section>
   </main>
 </template>
