9679621deeeb6c189baae6e15ee3bbee1d3719c9
Kevin Guibert <kevin.gbt2805@gmail.com>
2026-01-16 12:10:54 +0100

    formulaire inscription

--- frontend/src/views/Rejoindre.vue
+++ frontend/src/views/Rejoindre.vue
@@ -1,12 +1,35 @@
 <script setup>
-// Ici vous pourrez mettre la logique de cette page plus tard
+import { ref } from "vue";
+
+const pseudo = ref("");
+const date_naissance = ref("");
+const mail = ref("");
+const pays = ref("");
+
+async function submit() {
+  const res = await fetch("/api/participant", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      pseudo: pseudo.value,
+      date_naissance: date_naissance.value,
+      mail: mail.value,
+      pays: pays.value,
+    }),
+  });
+
+  const data = await res.json();
+  message.value = data.status;
+}
 </script>
 
 <template>
   <main>
     <section class="hero">
       <div class="hero-content">
-        <h1>Tu souhaites rejoindre <br>le tournoi ?</h1>
+        <h1>Tu souhaites rejoindre <br />le tournoi ?</h1>
         <h3>
           Alors inscris toi juste en dessous en rentrant toutes les informations
           nécessaires et participe au prochain tournoi !
@@ -14,11 +37,14 @@
       </div>
     </section>
     <section class="formulaire">
-      <label for="">Pseudo</label>
-      <label for="">Date de naissance</label>
-      <label for="">Mail</label>
-      <label for="">Pays</label>
-      <button class="btn btn-primary size-lg">S'inscrire</button>
+      <form @submit.prevent="submit">
+        <input v-model="pseudo" placeholder="Pseudo" />
+        <input v-model="date_naissance" placeholder="Date de Naissance" />
+        <input v-model.number="mail" placeholder="Mail" />
+        <input v-model.number="pays" placeholder="Pays" />
+        <button class="btn btn-primary size-lg">S'inscrire</button>
+      </form>
+      <p>{{ message }}</p>
     </section>
   </main>
 </template>
